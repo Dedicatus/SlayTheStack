c6cf008a9c56334a34130ca57311239e66f23105
haruorange <44792672+haruorange@users.noreply.github.com>
2020-04-15 22:46:03 -0700

    TowerShield
    Function call reconstruction

--- Assets/Scripts/TowerShield.cs
+++ Assets/Scripts/TowerShield.cs
@@ -8,19 +8,22 @@ public class TowerShield : MonoBehaviour
 	[SerializeField] private float yOffset;
 	//[SerializeField] private GameObject tower;
 	private Tower myTowerScript;
+	private TowerScroll myTowerScrollScript;
 	private float currentHeight;
 
-    // Start is called before the first frame update
-    void Start()
+
+	// Start is called before the first frame update
+	void Start()
     {
 		currentArmor = 0;
 		myTowerScript = transform.parent.GetComponent<Tower>();
+		myTowerScrollScript = transform.parent.GetComponent<TowerScroll>();
 	}
 
     // Update is called once per frame
     void Update()
     {
-		currentHeight = myTowerScript.getCurHeight() + transform.GetComponent<BoxCollider>().size.y + yOffset;
+		currentHeight = myTowerScript.getCurHeight() + transform.GetComponent<BoxCollider>().size.y + yOffset - myTowerScrollScript.getScrolledHeight();
 		gameObject.transform.position = new Vector3(transform.position.x, currentHeight, transform.position.z);
 
 		if (currentArmor > 0)
